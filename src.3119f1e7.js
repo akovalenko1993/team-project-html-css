f6a7a9919ce3e3f2b46e4cdd1521cd6e279ab9cf
akovalenko1993 <91023700+akovalenko1993@users.noreply.github.com>
2022-01-14 15:24:39 +0000

    Deploying to gh-pages from @ akovalenko1993/team-project-html-css@0c5e48f3c8556fbebac74b4f8f75988256011026 🚀

--- src.3119f1e7.js
+++ src.3119f1e7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),s;
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-html-css/src.8f32e52d.js.map
+//# sourceMappingURL=/team-project-html-css/src.3119f1e7.js.map
