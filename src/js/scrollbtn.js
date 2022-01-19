cf280900c73bd0b9406a9ef258e614f175d7ab51
AlexandrRust <123_00@ukr.net>
2022-01-19 12:49:34 +0200

    scroll-btn

---
+++ src/js/scrollbtn.js
@@ -0,0 +1,16 @@
+(() => {
+
+const scrollBtn = document.querySelector('.scroll-top')
+
+window.onscroll = () => {
+    if (window.scrollY > 410) {
+        scrollBtn.classList.remove('scroll-top_hide');
+    } else if (window.scrollY < 410) {
+        scrollBtn.classList.add('scroll-top_hide');
+    }
+    };
+    
+    scrollBtn.onclick = () => {
+        window.scrollTo(0, 0);
+}
+})();
